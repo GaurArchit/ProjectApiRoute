bd9267a2e414b02fa6c6541810556b8d8078f251
Archit <archit@MacBook-Pro.local>
2025-07-23 22:58:37 +0530

    Connect with MongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,12 +1,20 @@
-export default function handler(req,res){
-    if(req.method==='POST'){
-        const userEmail= req.body.email;
-        if(!userEmail || !userEmail.includes('@')){
-            res.status(422).json({message:'Invalid email adress'})
-            
-            return ;
-        }
-        console.log(userEmail);
-        res.status(200).json({message:`Signed up ${userEmail}`});
+import { MongoClient } from "mongodb";
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email adress" });
+
+      return;
     }
+  const client= await  MongoClient.connect(
+      "mongodb+srv://admin:admin@practiceapi.05fedit.mongodb.net/?retryWrites=true&w=majority&appName=PracticeAPI"
+    )
+      const db = client.db("myDatabaseName"); //by this we create the database in mongodb
+     await db.collection('emails').insertOne({email:userEmail})
+
+    client.close();
+    console.log(userEmail);
+    res.status(200).json({ message: `Signed up ${userEmail}` });
+  }
 }
